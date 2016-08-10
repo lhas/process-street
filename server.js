642203c159df021497d50d0a46cee763335a8a02
lhas <luizhrqas@gmail.com>
2016-08-10 01:13:47 -0300

    expressJS server to host uploaded files (alternative method)

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var multer  = require('multer')
+var upload = multer({ dest: './uploads/' })
+
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+})
+
+app.post('/', upload.single('avatar'), function (req, res, next) {
+  return res.send(req.file.filename);
+})
+
+app.listen(3010, function () {
+  console.log('Example app listening on port 3010!');
+})
